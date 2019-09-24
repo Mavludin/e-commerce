cc46ffd28a600ff69630e7d2732d7867b2f9a7a7
Mevlud <imevlud@gmail.com>
2019-09-24 20:41:33 +0300

    done some changes

--- js/script.js
+++ js/script.js
@@ -3,34 +3,37 @@ function newElement(productObj) {
 	var clothes = document.getElementById('clothes');
 	var accessories = document.getElementById('accessories');
 
-	var figure = document.createElement('figure');
-	var cardLink = document.createElement('a');
-	var image = document.createElement('img');
-	var figcaption = document.createElement('figcaption');
-	var heading = document.createElement('h4');
-	var par1 = document.createElement('p');
-	var par2 = document.createElement('p');
+	if (productObj.preview.match('.jpg')) {
 
-	image.src = productObj.preview;
-	cardLink.href = "details.html?product-id=" + productObj.id;
-	cardLink.appendChild(image);
-	var text = document.createTextNode(productObj.name);
-	heading.appendChild(text);
-	par1.className = "desc";
-	par2.className = "price";
-	var par1Text = document.createTextNode(productObj.brand);
-	var par2Text = document.createTextNode(productObj.price);
-	par1.appendChild(par1Text);
-	par2.appendChild(par2Text);
+		var figure = document.createElement('figure');
+		var cardLink = document.createElement('a');
+		var image = document.createElement('img');
+		var figcaption = document.createElement('figcaption');
+		var heading = document.createElement('h4');
+		var par1 = document.createElement('p');
+		var par2 = document.createElement('p');
 
-	figure.appendChild(cardLink);
-	figure.appendChild(figcaption);
-	figcaption.appendChild(heading);
-	figcaption.appendChild(par1);
-	figcaption.appendChild(par2);
-	
-	if (productObj.isAccessory === false) clothes.appendChild(figure);
-	if (productObj.isAccessory === true) accessories.appendChild(figure);
+		image.src = productObj.preview;
+		cardLink.href = "details.html?product-id=" + productObj.id;
+		cardLink.appendChild(image);
+		var text = document.createTextNode(productObj.name);
+		heading.appendChild(text);
+		par1.className = "desc";
+		par2.className = "price";
+		var par1Text = document.createTextNode(productObj.brand);
+		var par2Text = document.createTextNode(productObj.price);
+		par1.appendChild(par1Text);
+		par2.appendChild(par2Text);
+
+		figure.appendChild(cardLink);
+		figure.appendChild(figcaption);
+		figcaption.appendChild(heading);
+		figcaption.appendChild(par1);
+		figcaption.appendChild(par2);
+
+		if (productObj.isAccessory === false) clothes.appendChild(figure);
+		if (productObj.isAccessory === true) accessories.appendChild(figure);
+	}
 }
 
 function getData() {
